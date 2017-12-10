07203737b0abc58e3c47701ffc7dedbab02739f1
Leandro Eduardo <leandro-ebc@hotmail.com>
2017-12-10 10:44:11 -0200

    Adicionado campos logchamado
    alteradoTrigger

--- RadioTaxi2.0/sql/createInsertsUsers.sql
+++ RadioTaxi2.0/sql/createInsertsUsers.sql
@@ -262,14 +262,36 @@ grant select on num_Chamado_seq to gerente;
 grant gerente to gerenteUser;
 
 -- Trigger Log de Chamado
-create table logChamado(login varchar(20), data varchar(11), Hora varchar(11), cod_conveniada number(5));
+
+drop table logChamado;
+create table logChamado(
+	login varchar(20), 
+	data varchar(11), 
+	Hora varchar(11),
+	operacao varchar2(20),
+	num_boleto number(11),
+	cod_conveniada number(5)
+);
 
 create or replace trigger TRG_AFT_INS_UPD_DEL_CHAMADO
 after insert or update or delete on chamado
 for each row
 begin
-    insert into logChamado (login, data, Hora, cod_conveniada)
-        select user, sysdate as data, to_char(sysdate, 'hh24:mi:ss') as "Time", :new.cod_conveniada from dual;
+	if inserting then
+		insert into logChamado (login, data, Hora, operacao, num_boleto, cod_conveniada)
+        select user, sysdate as data, to_char(sysdate, 'hh24:mi:ss') as "Time", 'insert', :new.num_boleto, :new.cod_conveniada from dual;
+	end if;
+	
+	if deleting then
+		insert into logChamado (login, data, Hora, operacao, num_boleto, cod_conveniada)
+        select user, sysdate as data, to_char(sysdate, 'hh24:mi:ss') as "Time", 'delete', :old.num_boleto, :old.cod_conveniada from dual;
+	end if;
+		
+	if updating then
+		insert into logChamado (login, data, Hora, operacao, num_boleto, cod_conveniada)
+        select user, sysdate as data, to_char(sysdate, 'hh24:mi:ss') as "Time", 'update', :new.num_boleto, :new.cod_conveniada from dual;
+	end if;
+	
 end TRG_AFT_INS_UPD_DEL_CHAMADO;
 
 --Trigger Log detalhes chamado
